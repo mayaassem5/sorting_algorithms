b338a35298f7f2c26f9c1cffc390684780963131
mayaassem5 <mayaassem5@gmail.com>
2024-02-16 14:30:14 +0200

    edit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,50 @@
 #include "sort.h"
+
+
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * swap - swaps two integers in an array
+ * @a: first number
+ * @b: second number
+ */
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort -  an implementation of bubble sort
+ * @array: the array to be sorted
+ * @size: the size of the array
+ *
  */
 void bubble_sort(int *array, size_t size)
 {
+	unsigned int i = 0, j = 0;
+	unsigned int swapped = 0;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
+	if (!array || size < 2)
 		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+
+	while (j < size)
+	{
+		i = 0;
+		while (i < size - 1)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[i] > array[i + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				swap(array + i, array + i + 1);
+				swapped = 1;
+				print_array(array, size);
 			}
+			else
+				i++;
 		}
+		/*incase it was already sorted*/
+		if (!swapped)
+			return;
+		j++;
+	}
 }
--- sort.h
+++ sort.h
@@ -1,7 +1,8 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef SORTING_H
+#define SORTING_H
 
-#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -16,20 +17,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void quick_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **);
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-listint_t *swap_node(listint_t *node, listint_t **list);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
 
 #endif
